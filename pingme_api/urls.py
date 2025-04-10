0aad97813e4ffe0de4b6f686fd644e2e3f82fa16
shaAnder <shaunanderton1@gmail.com>
2025-04-10 12:49:38 +0100

    add authentication, rest ui login/logout

--- pingme_api/urls.py
+++ pingme_api/urls.py
@@ -25,6 +25,7 @@ from .views import CurrentUserAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,26 @@
-from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
+from django.http import Http404
 from .models import Profile
 from .serializers import ProfileSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from pingme_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(ListAPIView):
     """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
+    List all profiles.
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+class ProfileDetail(RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile.
+    Only the owner can update.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
